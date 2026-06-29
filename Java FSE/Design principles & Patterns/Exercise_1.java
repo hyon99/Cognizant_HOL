b504109fa425c2bd630465c4604a77c3703fd8aa
Sudipon Makal <57806842+hyon99@users.noreply.github.com>
2026-06-29 12:05:59 +0530

    Update Exercise_1.java

--- Java FSE/Design principles & Patterns/Exercise_1.java
+++ Java FSE/Design principles & Patterns/Exercise_1.java
@@ -1,30 +1,40 @@
-//Implementing the Singleton Pattern
-public class Exercise_1 {
-    static class Singleton 
-    {
-        private static Singleton instance;
-        private Singleton() 
-        {
-            System.out.println("Singleton Instance Created");
-        }
-        public static Singleton getInstance() 
-        {
-            if (instance == null) 
-            {
-                instance = new Singleton();
-            }
-            return instance;
-        }
-        public void showMessage() 
-        {
-            System.out.println("Hello from Singleton Pattern!");
-        }
+class Logger {
+
+    // Single instance of Logger
+    private static Logger instance;
+
+    // Private constructor
+    private Logger() {
     }
-    public static void main(String[] args) 
-    {
-        Singleton obj1 = Singleton.getInstance();
-        Singleton obj2 = Singleton.getInstance();
-        obj1.showMessage();
-        System.out.println("Are both objects same? " + (obj1 == obj2));
+
+    // Method to get the single instance
+    public static Logger getInstance() {
+        if (instance == null) {
+            instance = new Logger();
+        }
+        return instance;
+    }
+
+    // Logging method
+    public void log(String message) {
+        System.out.println("Log: " + message);
+    }
+}
+
+public class SingletonPatternExample {
+
+    public static void main(String[] args) {
+
+        Logger logger1 = Logger.getInstance();
+        Logger logger2 = Logger.getInstance();
+
+        logger1.log("Application Started");
+        logger2.log("Logging Data");
+
+        if (logger1 == logger2) {
+            System.out.println("Only one Logger instance exists.");
+        } else {
+            System.out.println("Multiple Logger instances exist.");
+        }
     }
 }
